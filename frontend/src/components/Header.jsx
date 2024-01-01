b887af7befaa1defc44b88e6e978361413d77b8f
Nivedit1234 <75367309+Nivedit1234@users.noreply.github.com>
2024-01-01 22:44:08 +0530

    Changes

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -24,6 +24,7 @@ const Header = () => {
                     <LinkContainer to='/login'>
                     <Nav.Link><FaUser/>Sign In</Nav.Link>
                     </LinkContainer>
+                    
                 </Nav>
               </Navbar.Collapse> 
             </Container>
