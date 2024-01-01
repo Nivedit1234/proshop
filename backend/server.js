81cb6ace9c29c945ac4f065e085d4ec66108e1f1
Nivedit1234 <75367309+Nivedit1234@users.noreply.github.com>
2024-01-01 16:25:14 +0530

    git ignore changed

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,6 @@ import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
-//import router from './routes/productRoutes.js';
 
 dotenv.config();
 connectDB(); //Connect to DB
@@ -15,17 +14,17 @@ app.get('/', (req, res) => {
   res.send('API is running...');
 });
 
-//app.use('/api/products', productRoutes);
+app.use('/api/products', productRoutes);
 
-app.get('/', (req, res) => {
-  res.json(products);
-});
+// app.get('/', (req, res) => {
+//   res.json(products);
+// });
 
-app.get('/:id', (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
+// app.get('/:id', (req, res) => {
+//   const product = products.find((p) => p._id === req.params.id);
 
-  res.json(product);
-});
+//   res.json(product);
+// });
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
