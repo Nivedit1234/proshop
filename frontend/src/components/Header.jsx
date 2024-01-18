cca64bbe61e8c632754fce31689f92ddcf529508
Nivedit1234 <75367309+Nivedit1234@users.noreply.github.com>
2024-01-18 21:09:59 +0530

    Profile and Logout icon display in Header done

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,4 +1,4 @@
-import {Badge,Navbar,Nav,Container} from 'react-bootstrap';
+import {Badge,Navbar,Nav,Container, NavDropdown} from 'react-bootstrap';
 import {FaShoppingCart,FaUser} from 'react-icons/fa';
 import Logo from '../assets/styles/logo.png';
 import {LinkContainer} from 'react-router-bootstrap';
@@ -8,6 +8,12 @@ import { useSelector } from 'react-redux';
 
 const Header = () => {
     const {cartItems } =useSelector((state)=>state.cart)
+    const {userInfo } =useSelector((state)=>state.auth)
+
+    const logoutHandler=()=>{
+      console.log("logoutHandler")
+    }
+
     console.log(cartItems)
     return (
     <header>
@@ -29,9 +35,17 @@ const Header = () => {
                       {cartItems.reduce((a,c)=>a+(c.qty),0)}
                     </Badge>)}</Nav.Link>
                     </LinkContainer>
-                    <LinkContainer to='/login'>
+                    {userInfo ? 
+                    (<NavDropdown title={userInfo.name} id='username'>
+                      <LinkContainer to='/profile'>
+                        <NavDropdown.Item>Profile</NavDropdown.Item>
+                      </LinkContainer>
+                      <NavDropdown.Item onClick={logoutHandler}>Logout</NavDropdown.Item>
+
+                    </NavDropdown>) : (<LinkContainer to='/login'>
                     <Nav.Link><FaUser/>Sign In</Nav.Link>
-                    </LinkContainer>
+                    </LinkContainer>)}
+                    
                     
                 </Nav>
               </Navbar.Collapse> 
