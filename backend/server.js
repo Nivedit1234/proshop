e36766571eb55117683ee807fe8d2d7bcac376aa
Nivedit1234 <75367309+Nivedit1234@users.noreply.github.com>
2024-01-23 21:30:03 +0530

    server.js changed

--- backend/server.js
+++ backend/server.js
@@ -53,22 +53,22 @@ app.use('/api/config/paypal', payPalFunc);
 const __dirname = path.resolve(); //Set __dirname to current directory
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
-if (process.env.NODE_ENV === 'production') {
-  const __dirname = path.resolve();
-  //app.use('/uploads', express.static('/var/data/uploads'));
-  app.use(express.static(path.join(__dirname, '/frontend/build')));
+// if (process.env.NODE_ENV === 'production') {
+//   const __dirname = path.resolve();
+//   //app.use('/uploads', express.static('/var/data/uploads'));
+//   app.use(express.static(path.join(__dirname, '/frontend/build')));
 
-  app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
-  );
-} else {
-  const __dirname = path.resolve();
-  //app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
-  //if you are not in production we are running rect dev server
-  app.get('/', (req, res) => {
-    res.send('API is running....');
-  });
-}
+//   app.get('*', (req, res) =>
+//     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+//   );
+// } else {
+//   const __dirname = path.resolve();
+//   //app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+//   //if you are not in production we are running rect dev server
+//   app.get('/', (req, res) => {
+//     res.send('API is running....');
+//   });
+// }
 
 app.use(errorHandler);
 app.use(notFound);
